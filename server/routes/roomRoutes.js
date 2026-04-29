0651bb8325f8fffb010e26e71fc9d22374413603
Malky <mk0533159433@gmail.com>
2026-04-29 13:31:31 +0300

    add clearAllRoomsPlacements function

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createRoom, deleteRoom, findFirstAvailableRoom, getAllRooms, getRoomById, updateRoom, clearRoomPlacements } from '../Controller/roomController.js';
+import { createRoom, deleteRoom, findFirstAvailableRoom, getAllRooms, getRoomById, updateRoom, clearRoomPlacements, clearAllRoomsPlacements } from '../Controller/roomController.js';
 
 const router = express.Router();
 
 // סדר קריטי! הספציפי (search) תמיד מעל הכללי (:id)
 router.get('/search', findFirstAvailableRoom);
+router.delete('/clear-all-placements', clearAllRoomsPlacements);
 router.get('/', getAllRooms);
 router.post('/', createRoom);
 router.get('/:id', getRoomById);
